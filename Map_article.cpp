7969ca8929b44d95b6c586cc124f765786eadf12
Seny SORET <seny.soret@viacesi.fr>
2022-12-05 09:07:29 +0100

    UPDATE

--- Map_article.cpp
+++ Map_article.cpp
@@ -8,30 +8,36 @@ Svc_Article::Map_Articles::Map_Articles(void)
 	this->nature = "rien";
 	this->nom_article = "rien";
 	this->couleur = "rien";
-	this->taux_TVA = 0,0;
-	this->prix_articleUHT = 0, 0;
-	this->remise_articles = 0, 0;
+	this->taux_TVA = 0.0;
+	this->prix_articleUHT = 0.0;
+	this->remise_articles = 0.0;
 }
 
 String^ Svc_Article::Map_Articles::SELECT(void)
 {
-	return;
+	throw gcnew System::NotImplementedException();
+	// TODO: insérer une instruction return ici
 }
 
 String^ Svc_Article::Map_Articles::INSERT(void)
 {
-	return;
+	throw gcnew System::NotImplementedException();
+	// TODO: insérer une instruction return ici
 }
 
 String^ Svc_Article::Map_Articles::UPDATE(void)
 {
-	return;
+	throw gcnew System::NotImplementedException();
+	// TODO: insérer une instruction return ici
 }
 
 String^ Svc_Article::Map_Articles::DELETE(void)
 {
-	return;
+	throw gcnew System::NotImplementedException();
+	// TODO: insérer une instruction return ici
 }
+
+
 
 int Svc_Article::Map_Articles::getRef_Article(void)
 {
@@ -120,7 +126,7 @@ void Svc_Article::Map_Articles::setStock_Articles(int stock_articles)
 
 void Svc_Article::Map_Articles::setRemise_Articles(float remise_articles)
 {
-	if (remise_articles > 0,0)
+	if (remise_articles > 0)
 	{
 		this->remise_articles = remise_articles;
 	}
@@ -128,7 +134,7 @@ void Svc_Article::Map_Articles::setRemise_Articles(float remise_articles)
 
 void Svc_Article::Map_Articles::setPrix_ArticleUHT(float prix_articleUHT)
 {
-	if (prix_articleUHT > 0, 0)
+	if (prix_articleUHT > 0)
 	{
 		this->prix_articleUHT = prix_articleUHT;
 	}
@@ -136,7 +142,7 @@ void Svc_Article::Map_Articles::setPrix_ArticleUHT(float prix_articleUHT)
 
 void Svc_Article::Map_Articles::setTaux_TVA(float taux_TVA)
 {
-	if (taux_TVA > 0, 0)
+	if (taux_TVA > 0)
 	{
 		this->taux_TVA = taux_TVA;
 	}
